6d232e238e698882d60598ec53306089d7921e3c
egidijus <145454567+egidijus1989@users.noreply.github.com>
2024-02-10 18:33:17 +0200

    liko tik aprasymas

--- controllers/expenditureController.js
+++ controllers/expenditureController.js
@@ -1,75 +1,123 @@
-const fs = require('fs')
-const Expenditure = require('./../models/expenditureModel')
+const fs = require("fs");
+const Expenditure = require("./../models/expenditureModel");
 
 //Callbaks\\////////////////////////////////
 
 //GetAllExpenditures\\//////////////////////
-exports.getAllExpenditures = async (req, res)=>{
-    try{
-        const expenditures = await Expenditure.find()
-        res.status(200).json({
-            status: "success",
-            results: `Is viso yra: ${expenditures.length} islaidu`,
-            data: {expenditures}
-        })
-    }catch(err){
-        
-    }
-}
+exports.getAllExpenditures = async (req, res) => {
+  try {
+    const expenditures = await Expenditure.find();
+    res.status(200).json({
+      status: "success",
+      results: `Is viso yra: ${expenditures.length} islaidos`,
+      data: { expenditures },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+
 //GetExpendituresByDate\\///////////////////
+exports.getAllExpendituresFromTo = async (req, res) => {
+  try {
+    const expenditures = await Expenditure.find({
+      date: { $gte: new Date(req.params.from), $lt: new Date(req.params.to) },
+    });
+    res.status(200).json({
+      status: "success",
+      results: `Duotuoju laikotarpiu is viso yra: ${expenditures.length} islaidu`,
+      data: { expenditures },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
 
 //GetExpendituresByDateSum\\////////////////
-
+exports.getAllExpendituresFromToSum = async (req, res) => {
+  try {
+    const expenditures = await Expenditure.find({
+      date: { $gte: new Date(req.params.from), $lt: new Date(req.params.to) },
+    });
+    // console.log(expenditures);
+    // console.log(req.params);
+    let sum = 0;
+    for (const expenditure of expenditures) {
+      sum += expenditure.amount;
+    }
+    console.log(sum);
+    res.status(200).json({
+      status: "success",
+      results: `Duotuoju laikotarpiu islaidu buvo: ${expenditures.length}`,
+      data: { sum },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
 //CreateExpenditure\\///////////////////////
-exports.createExpenditure = async (req, res) =>{
-    try{
-        const newExpenditure = await Expenditure.create(req.body)
-        res.status(201).json({
-            status: "success",
-            message: "Expenditure is added",
-            data: {newExpenditure}
-        })
-    }catch(err){
-        res.status(404).json({
-            status: "fail",
-            message:err
-        })
-    }
-}
+exports.createExpenditure = async (req, res) => {
+  try {
+    const newExpenditure = await Expenditure.create(req.body);
+    res.status(201).json({
+      status: "success",
+      message: "Expenditure is added",
+      data: { newExpenditure },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
 //UpdateExpenditure\\///////////////////////
-exports.updateExpenditure = async (req, res) =>{
-    try{
-        const expenditure = await Expenditure.findByIdAndUpdate(req.params.id, req.body,{
-            new:true,
-            runValidators: true
-        })
-        res.status(200).json({
-            status: "success",
-            message: "Expenditure is updated",
-            data: {expenditure}
-        })
-    }catch(err){
-        res.status(404).json({
-            status: "fail",
-            message:err
-        })
-    }
-}
+exports.updateExpenditure = async (req, res) => {
+  try {
+    const expenditure = await Expenditure.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    res.status(200).json({
+      status: "success",
+      message: "Expenditure is updated",
+      data: { expenditure },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
 
 //DeleteExpenditure\\///////////////////////
-exports.deleteExpenditure = async (req, res) =>{
-    try{
-        await Expenditure.findByIdAndDelete(req.params.id)
-        res.status(200).json({
-            status: "success",
-            message: "expenditure is deleted",
-            data: null
-        })
-    }catch(err){
-        res.status(404).json({
-            status: "fail",
-            message:err
-        })
-    }
-}
+exports.deleteExpenditure = async (req, res) => {
+  try {
+    await Expenditure.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      status: "success",
+      message: "expenditure is deleted",
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
 /////////////////////////////////////////////
--- routes/expenditureRoutes.js
+++ routes/expenditureRoutes.js
@@ -1,19 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const expenditureController = require('../controllers/expenditureController')
+const expenditureController = require("../controllers/expenditureController");
 
-router.route('/')
-.get(expenditureController.getAllExpenditures)
-.post(expenditureController.createExpenditure)
+router
+  .route("/")
+  .get(expenditureController.getAllExpenditures)
+  .post(expenditureController.createExpenditure);
 
-router.route('/:id')
-.patch(expenditureController.updateExpenditure)
-.delete(expenditureController.deleteExpenditure)
+router
+  .route("/:id")
+  .patch(expenditureController.updateExpenditure)
+  .delete(expenditureController.deleteExpenditure);
 
-// router.route('/period')
-// .get(expenditureController.getAllExpenditures)
+router.route("/:from/:to").get(expenditureController.getAllExpendituresFromTo);
 
-// router.route('/period-sum')
-// .get(expenditureController.getAllExpenditures)
+router
+  .route("/sum/:from/:to")
+  .get(expenditureController.getAllExpendituresFromToSum);
 
-module.exports = router
+module.exports = router;
